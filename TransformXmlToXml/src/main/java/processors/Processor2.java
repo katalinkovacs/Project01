a79ba68093ced8af0942f15994aed09fbe833c91
katalinkovacs <katalin.kovacs@marlo.com.au>
2017-02-27 12:36:05 +1100

    Created Route2, Processor2, StartApp2. Transform employee2 to employee1. File name is not correct??????????????

---
+++ TransformXmlToXml/src/main/java/processors/Processor2.java
@@ -0,0 +1,33 @@
+package processors;
+
+import employeeversion1.Employeeversion1;
+import employeeversion2.Employeeversion2;
+import org.apache.camel.Exchange;
+
+public class Processor2 {
+
+    // processor1Method method --> argument (Exchange ex)
+
+    public void processor2Method (Exchange ex) throws Exception {
+
+
+        ex.getIn().setHeader("FileComponent.HEADER_FILE_NAME", "Employee2toEmployee1.xml");
+
+        Employeeversion2 e2 = (Employeeversion2) ex.getIn().getBody();
+
+
+        String firstName = e2.getGivenname();
+        String lastName = e2.getFamilyname();
+
+
+        Employeeversion1 e1 = new Employeeversion1();
+
+        // add the retrieved data from p1 to p2
+        e1.setLastname(lastName);
+        e1.setFirstname(firstName);
+
+
+        ex.getIn().setBody(e1);
+
+    }
+}
--- TransformXmlToXml/src/main/java/routes/Route2.java
+++ TransformXmlToXml/src/main/java/routes/Route2.java
@@ -1,5 +1,43 @@
 package routes;
 
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.converter.jaxb.JaxbDataFormat;//
+import processors.Processor2;
 
-public class Route2 {
+
+public class Route2 extends RouteBuilder{
+
+    Processor2 processor2Object = new Processor2();
+
+    public void configure() throws Exception {
+
+
+        /* DataFormat jaxb = new JaxbDataFormat("com.acme.model");
+         *  from("activemq:My.Queue").
+         *       unmarshal(jaxb).
+         *       to("mqseries:Another.Queue");
+         */
+
+
+        JaxbDataFormat jaxbDataFormat2 = new JaxbDataFormat("employeeversion2");
+
+        JaxbDataFormat jaxbDataFormat1 = new JaxbDataFormat("employeeversion1");
+
+
+        // FROM FILE TO FILE
+
+        from("file:C:/Kati/Marlo/GitRepo/FileReads/Project01/From2")
+                //unmarshalling is the process to read in xml and convert to java object using jaxb generated template
+                // classes
+                .unmarshal(jaxbDataFormat2)
+                //this is to invoke processor to transform person1 to person2
+                .bean(processor2Object, "processor2Method")   // object + method
+                //marshalling is the process to convert java to xml -- the processor from previous step returns
+                // a person2 which will be used to create the output xml
+                .marshal(jaxbDataFormat1)
+                .to("file:C:/Kati/Marlo/GitRepo/FileReads/Project01/To2");
+
+    }
+
 }
+
