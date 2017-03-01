17b6826f9213cc9b4ba41eedc3be759b560269ff
katalinkovacs <katalin.kovacs@marlo.com.au>
2017-03-01 16:42:12 +1100

    Changed itignore.......RESTORING!!!!

---
+++ TransformXmlToXml/src/main/java/processors/Processor4.java
@@ -0,0 +1,46 @@
+package processors;
+
+import employees1_phone.Employeeversion1;
+import employees2_phone.Employeeversion2;
+import org.apache.camel.Exchange;
+
+
+public class Processor4 {
+
+    public void processor4Method (Exchange ex) throws Exception {
+
+        ex.getIn().setHeader("CamelFileName", "Employeephone1toEmployeephone2.xml");
+
+        Employeeversion1 e1 = (Employeeversion1) ex.getIn().getBody();
+
+        //create output xml structure top level
+        Employeeversion2 employeeversion2 = new Employeeversion2();
+
+        // second level
+        Employeeversion2.Staff staff = new Employeeversion2.Staff();
+        employeeversion2.setStaff(staff);
+
+
+        // third level??????????????????
+        Employeeversion2.Staff.Staffmember staffmember = new Employeeversion2.Staff.Staffmember();
+        //set????????????//
+
+
+
+
+        // looping through the input list with list item type  here the list item type is Employeeversion1.Employees.Employee
+        // and the list is from the input body
+        for(Employeeversion1.Employees.Employee employeeIn : e1.getEmployees().getEmployee()){
+
+            Employeeversion2.Staff.Staffmember employeeOut = new Employeeversion2.Staff.Staffmember();
+
+            employeeOut.setFamilyname(employeeIn.getLastname());
+            employeeOut.setGivenname(employeeIn.getFirstname());
+            staff.getStaffmember().add(employeeOut);
+
+
+        }
+        ex.getIn().setBody(employeeversion2);
+        System.out.print("hello");
+    }
+}
